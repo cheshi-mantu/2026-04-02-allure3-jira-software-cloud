7fe2753120fc2cf87960067ab94c468761067511
Egor I <egor.ivanov@pm.me>
2026-03-24 18:45:49 +0100

    adds Allure3 JSC plugin
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- allurerc.mjs
+++ allurerc.mjs
@@ -1,6 +1,5 @@
 import { defaultChartsConfig, defineConfig } from "allure";
 
-
 export default defineConfig({
   name: "Allure 3 Report Example",
   output: "./allure-report",
@@ -16,7 +15,16 @@ export default defineConfig({
         groupBy: ["feature"],
         charts: chartLayout,
       },
+      jira: {
+        options: {
+          issue: process.env.ALLURE_JIRA_ISSUE, 
+          webhook: process.env.ALLURE_JIRA_WEBHOOK,
+          token: process.env.ALLURE_JIRA_TOKEN,
+          uploadReport: true,
+          uploadResults: true,
+        },
       },
+    },
   },
   variables: {
     env_variable: "unknown",
